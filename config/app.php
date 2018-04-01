2d6a61261632d825572310f5fd35a0cb9a7686a2
unknown <nikolastps@hotmail.com>
2018-04-01 17:45:01 -0300

    Forms & Saving data

--- config/app.php
+++ config/app.php
@@ -177,6 +177,16 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * HTML & Forms
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
+        /*
+         * CKEditor
+         */
+        Unisharp\Ckeditor\ServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +235,12 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * HTML & Forms
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
