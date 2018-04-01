87aea1ced4c8f437d3238871ab7e1808b02ee5be
unknown <nikolastps@hotmail.com>
2018-03-31 22:58:20 -0300

    Blade Templating & Compiling Assets

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,18 +9,24 @@ class PagesController extends Controller
     //Chama a pagina principal
     public function index()
     {
-        return view('pages.index');
+        $title = 'Welcome to Laravel!';
+        return view('pages.index')->with('title', $title);
     }
 
     //Chama a pagina sobre
     public function about()
     {
-        return view('pages.about');
+        $title = 'About Us';
+        return view('pages.about')->with('title', $title);
     }
 
     //Chama a pagina de serviços
     public function services()
     {
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
     }
 }
